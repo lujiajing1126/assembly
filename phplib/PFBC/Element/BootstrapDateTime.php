2332e3e5a991c93ff4ba133e9978fbfe0a987b8a
Sherwood Wang <SWang.Work@gmail.com>
2013-05-07 17:53:28 +0800

    debug PFBC element BootstrapDateTime

--- phplib/PFBC/Element/BootstrapDateTime.php
+++ phplib/PFBC/Element/BootstrapDateTime.php
@@ -3,21 +3,20 @@ namespace PFBC\Element;
 
 class BootstrapDateTime extends Textbox {
     protected $_attributes = array("type" => "text", "data-format" => "dd/MM/yyyy hh:mm:ss");
-    protected $append = '<i data-time-icon="icon-time" data-date-icon="icon-calendar"></i>'
+    protected $append = '<i data-time-icon="icon-time" data-date-icon="icon-calendar"></i>';
 
     public function getCSSFiles() {
-        if(!in_array("bootstrap-datetimepicker", $this->_form->prevent))
-            return array($this->_form->_prefix . "://su.fudan.edu.cn/bootstrap/css/bootstrap-datetimepicker.min.css");
+        if(!in_array("bootstrap-datetimepicker", $this->_form->getPrevent))
+            return array($this->_form->getPrefix . "bootstrap/css/bootstrap-datetimepicker.min.css");
     }
 
     public function getJSFiles() {
-        if(!in_array("bootstrap-datetimepicker", $this->_form->prevent))
-            return array($this->_form->_prefix . "://su.fudan.edu.cn/bootstrap/js/bootstrap-datetimepicker.min.js");
+        if(!in_array("bootstrap-datetimepicker", $this->_form->getPrevent))
+            return array($this->_form->getPrefix . "bootstrap/js/bootstrap-datetimepicker.min.js");
     }
 
     public function jQueryDocumentReady() {
         parent::jQueryDocumentReady();
-        echo 'jQuery("#', $this->_attributes["id"], '").datetimepicker(', $this->jQueryOptions(), ');';
+        echo 'jQuery("#', $this->_attributes["id"], '").parent().datetimepicker(', $this->jQueryOptions(), ');';
     }
 }
-
