5d40fddff8dc43b1f261583d437344b0e663bcfd
Sherwood Wang <SWang.Work@gmail.com>
2013-09-13 12:25:24 +0800

    add $LIBDIR variable to phplib/config.php

--- phplib/config.php
+++ phplib/config.php
@@ -3,4 +3,8 @@ $DSN = array (
         'activity' => 'pgsql:host=localhost;user=su;password=sudb;dbname=su_main',
         'announcement' => 'pgsql:host=localhost;user=su;password=sudb;dbname=su_main'
 );
+
+$SERVER['DOCUMENT_ROOT'] = rtrim($SERVER['DOCUMENT_ROOT'], DIRECTORY_SEPARATOR);
+
+$LIBDIR = $SERVER['DOCUMENT_ROOT'] . '.lib';
 ?>
